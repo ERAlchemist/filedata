1d2760906a313f2712ebb4beffe8750e5a0bab70
Eric Albertus <eralchemist@gmail.com>
2019-03-25 00:28:58 -0700

    update .post() method

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.post('/', upload.single('file'), function(req, res, next) {
+router.post('/', upload.single('upfile'), function(req, res, next) {
   console.log("upload!!!");
   const { size } = req.file;
   console.log("up here!");
